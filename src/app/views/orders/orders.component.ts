73751eeb62244643a255429677b7122a8670fd7e
kek-Sec <georgepetrakis99@gmail.com>
2022-01-18 13:23:39 +0200

    foo mobile phones

--- src/app/views/orders/orders.component.ts
+++ src/app/views/orders/orders.component.ts
@@ -2,19 +2,70 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-orders',
-  template: `
-    <p>
-      orders works!
-    </p>
-  `,
+  template: ` <!--Create table to display orders-->
+    <table class="table table-stripped">
+      <thead style="color:#88c724">
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">User</th>
+          <th scope="col">Product</th>
+          <th scope="col">Date</th>
+        </tr>
+      </thead>
+      <tbody>
+        <ng-container *ngFor="let order of orders">
+          <tr>
+            <td class="col-xs-3">{{ order.orderID }}</td>
+            <td class="col-xs-3">{{ order.userID }}</td>
+            <td class="col-xs-3">{{ order.productID }}</td>
+            <td class="col-xs-6">{{ order.date }}</td>
+          </tr>
+        </ng-container>
+      </tbody>
+    </table>`,
   styles: [
-  ]
+    ` `,
+  ],
 })
 export class OrdersComponent implements OnInit {
+  //an order has
+  //orderID, productID, userID, date.
 
-  constructor() { }
+  //create foo data
+  orders = [
+    {
+      orderID: 1,
+      productID: 1,
+      userID: 1,
+      date: '2020-01-01',
+    },
+    {
+      orderID: 2,
+      productID: 2,
+      userID: 2,
+      date: '2020-01-02',
+    },
+    {
+      orderID: 3,
+      productID: 3,
+      userID: 3,
+      date: '2020-01-03',
+    },
+    {
+      orderID: 4,
+      productID: 4,
+      userID: 4,
+      date: '2020-01-04',
+    },
+    {
+      orderID: 5,
+      productID: 5,
+      userID: 5,
+      date: '2020-01-05',
+    },
+  ];
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
--- src/app/views/store/store.component.ts
+++ src/app/views/store/store.component.ts
@@ -2,19 +2,58 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-store',
-  template: `
-    <p>
-      store works!
-    </p>
-  `,
-  styles: [
-  ]
+  template: ` <p>store works!</p> `,
+  styles: [],
 })
 export class StoreComponent implements OnInit {
+  //the store will display a list of products.
+  //each product has
+  //productid, price, quantity, photoURL, model (π.χ. Xiaomi Mi11), screenSize, CPU, RAM,camera, battery, SAR
 
-  constructor() { }
+  //create foo data
+  products = [
+    {
+      productID: 1,
+      price: 100,
+      quantity: 10,
+      photoURL: '',
+      model: 'Xiaomi Mi11',
+      screenSize: '6.3',
+      CPU: 'Snapdragon 865',
+      RAM: '6GB',
+      camera: '12MP',
+      battery: '4000mAh',
+      SAR: '5.5',
+    },
+    {
+      productID: 2,
+      price: 200,
+      quantity: 20,
+      photoURL: '',
+      model: 'Samsung S10',
+      screenSize: '6.3',
+      CPU: 'Snapdragon 865',
+      RAM: '6GB',
+      camera: '12MP',
+      battery: '4000mAh',
+      SAR: '5.5',
+    },
+    {
+      productID: 3,
+      price: 300,
+      quantity: 30,
+      photoURL: '',
+      model: 'Xiaomi Mi11',
+      screenSize: '6.3',
+      CPU: 'Snapdragon 865',
+      RAM: '6GB',
+      camera: '12MP',
+      battery: '4000mAh',
+      SAR: '5.5',
+    },
+  ];
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
